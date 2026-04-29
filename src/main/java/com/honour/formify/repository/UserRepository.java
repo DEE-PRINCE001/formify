4fc4054f5006e6f62b448496b9301c68b93b5a70
Dee_Prince <ajanihonour@gmail.com>
2026-04-29 11:39:20 +0100

    added the applicationConfig

--- src/main/java/com/honour/formify/repository/UserRepository.java
+++ src/main/java/com/honour/formify/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.honour.formify.repository;
 
+import org.springframework.stereotype.Service;
+
 import com.honour.formify.entity.User;
 
+@Service
 public interface UserRepository extends org.springframework.data.jpa.repository.JpaRepository<User, Long> {
     java.util.Optional<User> findByEmail(String email);
    
